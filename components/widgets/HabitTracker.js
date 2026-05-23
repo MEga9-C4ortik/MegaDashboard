9ee39acf5b6c487e8371bb80296fbb60e254dc8d
Mega <megaovezov9@gmail.com>
2026-05-23 16:43:23 +0800

    add habit tracker

--- components/widgets/HabitTracker.js
+++ components/widgets/HabitTracker.js
@@ -1,8 +1,31 @@
+'use client';
 import {useState} from "react";
 
 
 function HabitTracker() {
+    const today = new Date().toISOString().split('T')[0];
     const [habits, setHabits] = useState([]);
+
+    const toggleHabit = (habit) => {
+        if(lastDate === today) {
+
+        }
+    }
+
+    return (
+        <div>
+            {habits.map(habit => (
+                <div key={habit.id} className="habit">
+                    <input className={""}
+                           type={"checkbox"}
+                           value={habit.name}
+                           onChange={()=> toggleHabit(habit)}
+                           checked={habit.done}/>
+                </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default HabitTracker;
