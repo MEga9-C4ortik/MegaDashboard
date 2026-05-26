bd8925d12ec4fc52dc3280e4eeda12d88e6f32f3
Mega <megaovezov9@gmail.com>
2026-05-26 16:50:23 +0800

    added manifest

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Dashboard",
-  description: "Mega",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
