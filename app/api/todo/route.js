285ff9fedeade5816720ebd08a325f48be34d16d
Mega <megaovezov9@gmail.com>
2026-05-28 16:07:33 +0800

    fixed Get notion api func

--- app/api/todo/route.js
+++ app/api/todo/route.js
@@ -3,7 +3,10 @@ import notion from '@/lib/notion.js';
 export async function GET() {
     const response = await notion.databases.query({
         database_id: process.env.NOTION_WEEK_PLAN,
-    });
 
-    return Response.json(response);
+    });
+    const currentWeek = response.results.find(page => page.properties.Status.status.name === "In progress");
+    const currentWeekId = currentWeek.id;
+    const blocks = await notion.blocks.children.list({ block_id: currentWeekId })
+    return Response.json(blocks.results);
 }
