d8568ae7f2c901352512d11b8479ee866bd92e8e
Mega <megaovezov9@gmail.com>
2026-05-29 22:05:20 +0800

    add Notes

---
+++ components/widgets/Notes.js
@@ -0,0 +1,11 @@
+'use client'
+import {useEffect, useState} from "react";
+
+function Notes() {
+    const [categories, setCategories] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [notes, setNotes] = useState([]);
+    const [cache, setCache] = useState({});
+
+
+}
