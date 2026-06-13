9b5ac8094f77dcb651bc8c590caaecb072ad4e8c
Mergen <mergenovezov987@gmail.com>
2026-06-13 18:05:39 +0800

    create Study and Hobby api, add GET func

---
+++ app/api/study/route.js
@@ -0,0 +1,34 @@
+import notion from "@/lib/notion";
+
+export async function GET () {
+    const response =  await notion.databases.query({
+        database_id: process.env.NOTION_STUDY,
+        filter: {
+            property: "Status",
+            status: {
+                does_not_equal:  "Done"
+            },
+        },
+        sorts: [
+            {
+                property: 'Deadline',
+                direction: 'ascending'
+            }
+        ]
+    });
+    const subjects = response.results;
+    if (subjects.length <= 0) return Response.json([], { status: 200 });
+
+    const mapped = subjects.map((subject) => { return {
+        id: subject.id,
+        url: subject.url,
+        name: subject.properties.Subject.title?.[0].plain_text,
+        deadline: subject.properties.Deadline.date.start,
+        tasks: subject.properties.Tasks.rich_text[0]?.plain_text,
+        description: subject.properties.Description.rich_text[0]?.plain_text,
+        status: subject.properties.Status.status.name,
+        grades: subject.properties.Grades.rich_text[0]?.plain_text
+    }});
+
+    return Response.json(mapped);
+}
